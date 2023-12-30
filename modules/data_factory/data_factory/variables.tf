1f7370caf48f0d71739af31429fb39c6a1556534
Sty-ven <oriyomistephen@gmail.com>
2023-12-30 11:09:51 +0100

    azurecicd pipeline

---
+++ modules/data_factory/data_factory/variables.tf
@@ -0,0 +1,21 @@
+variable "storage_account_name" {
+  description = "The name of the storage_account"
+  type = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type = string
+}
+
+variable "location" {
+  description = "The location/region of the resource"
+  type = string
+}
+
+variable "df_name" {
+  description = "The data factory name"
+  type = string
+}
+
+
---
+++ variables.tfvars
@@ -0,0 +1,12 @@
+resource_group_name = "steph-rg"
+storage_account_name = "stephsa"
+location = "uksouth"
+
+tags = {
+  Environment = "development"
+}
+
+source_folder_name = "source"
+destination_folder_name = "destination"
+
+df_name = "steph-adf"
